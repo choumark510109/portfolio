13499c3511c2d0e44fbc681b56cc66864e7141ab
choumark510109 <marks10820007@gmail.com>
2025-06-14 02:13:59 +0800

    Update markdown.js

--- js/markdown.js
+++ js/markdown.js
@@ -1,1 +1,19 @@
+.blog-post {
+  max-width: 700px;
+  margin: 5rem auto;
+  padding: 0 1.5rem;
+  font-family: "Noto Sans TC", sans-serif;
+  line-height: 1.8;
+  color: #fff;
+}
+
+.blog-post h1, h2, h3 {
+  margin-top: 2rem;
+  color: #f0c674;
+}
+
+.blog-post a {
+  color: #66d9ef;
+  text-decoration: underline;
+}
 
